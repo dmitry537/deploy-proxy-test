8820ee37e38baad79ce23ce3a17939fc24027e1f
Dmitry Kharlanchuk <kharlanchuk@scand.com>
2023-06-13 23:59:26 +0300

    vox added

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,9 +3,8 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "operator-filter-registry/src/DefaultOperatorFilterer.sol";
 
-contract Box is DefaultOperatorFilterer, OwnableUpgradeable {
+contract Box is OwnableUpgradeable {
 
     function initialize() public initializer {
         __Ownable_init();
---
+++ contracts/Vox.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "operator-filter-registry/src/DefaultOperatorFilterer.sol";
+
+contract Vox is DefaultOperatorFilterer {
+
+}
